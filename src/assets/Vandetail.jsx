00e30f3af9380f1037f77ba888f62fb0eceb4706
Ayush1M <worklife2442@gmail.com>
2023-05-01 02:34:34 +0530

    Added the van detail page.

---
+++ src/assets/Vandetail.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+export default function Vandetail(){
+    const params = useParams()
+    const [vanDetail, setVanDetail] = useState([])
+
+    useEffect(()=> {
+        fetch(`/api/vans/${params.id}`)
+        .then(res => res.json())
+        .then(data => setVanDetail(data.vans))
+    }, [params.id])
+    
+    console.log(vanDetail)
+
+    return(
+        <div className="van-detail-container">
+            {vanDetail ? (
+                <div className="van-detail">
+                    <img src={vanDetail.imageUrl} />
+                    <i className={`van-type ${vanDetail.type} selected`}>{vanDetail.type}</i>
+                    <h2>{vanDetail.name}</h2>
+                    <p className="van-price"><span>${vanDetail.price}</span>/day</p>
+                    <p>{vanDetail.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
+}
