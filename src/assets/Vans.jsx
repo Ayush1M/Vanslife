66cd36b5b529517fa2ccd01e8830ccb836ab95b1
Ayush1M <worklife2442@gmail.com>
2023-04-30 23:34:06 +0530

    fetched the vans data from server file.

--- src/assets/Vans.jsx
+++ src/assets/Vans.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 
 export default function Vans(){
+    const [vansData, setVansData] = useState([])
+
+    useEffect(()=> {
+        async function getVansData(){
+            const res = await fetch('/api/vans')
+            const data = await res.json()
+            setVansData(data.vans)
+        }
+        getVansData()
+    }, [])
+
+    const vanElements = vansData.map((vanData) => (
+              <div key={vanData.id} className="van-tile">
+               <img src={vanData.imageUrl} />
+              <div className="van-info">
+                <h3>{vanData.name}</h3>
+                <p>${vanData.price}<span>/day</span></p>
+              </div>
+                <i className={`van-type ${vanData.type} selected`}>{vanData.type}</i>
+             </div>
+    ))
+
     return(
         <>
-        <h1>hello</h1>
+        <div className="van-list-container">
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
         </>
+            
     )
 }
