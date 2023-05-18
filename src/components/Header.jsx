b75d6b0290fab56d040cfc95cd0cdfffcde2303d
Ayush1M <worklife2442@gmail.com>
2023-05-18 23:53:13 +0530

    Added navlink for styling links.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header(){
     return(
         <header>
           <Link className="site-logo" to="/">#vanlife</Link>
           <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
+            <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
+            <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
+            <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
           </nav>
        </header>
     )
