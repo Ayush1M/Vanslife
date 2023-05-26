f5a15345f0bacb68bb4f14c691c26f5282f26fbb
Ayush1M <worklife2442@gmail.com>
2023-05-26 23:46:27 +0530

    Added filter option for filtering type of vans.

--- src/assets/Vans.jsx
+++ src/assets/Vans.jsx
@@ -1,9 +1,13 @@
 import React, {useEffect, useState} from "react";
-import {Link} from 'react-router-dom'
+import {Link, useSearchParams} from 'react-router-dom'
 
 
 export default function Vans(){
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vansData, setVansData] = useState([])
+
+    const typeFilter = searchParams.get("type")
+    const displayVans = typeFilter ? vansData.filter((van) => van.type.toLowerCase() === typeFilter) : vansData
 
     useEffect(()=> {
         async function getVansData(){
@@ -14,7 +18,8 @@ export default function Vans(){
         getVansData()
     }, [])
 
-    const vanElements = vansData.map((vanData) => (
+
+    const vanElements = displayVans.map((vanData) => (
               <div key={vanData.id} className="van-tile">
             <Link to={`/vans/${vanData.id}`}>
                <img src={vanData.imageUrl} />
@@ -31,6 +36,18 @@ export default function Vans(){
     return(
         <>
         <div className="van-list-container">
+            <h1>Explore Our Van Options</h1>
+            <div className="van-list-filter-buttons">
+                <button onClick={() => setSearchParams({type: "simple"})} className={`van-type simple ${typeFilter === "simple" ? "selected" : ""} `}>Simple</button>
+
+                <button onClick={() => setSearchParams({type: "rugged"})} className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""} `}>Rugged</button>
+
+                <button onClick={() => setSearchParams({type: "luxury"})} className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""} `}>Luxury</button>
+
+                {typeFilter 
+                ? (<button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear Filter</button>) 
+                : null}
+            </div>
             {
                 vansData.length > 0 ? 
             (<div className="van-list">
