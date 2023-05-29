bc2609405afd6507e82b7da4846f703e062ecc12
Ayush1M <worklife2442@gmail.com>
2023-05-29 21:32:52 +0530

    Added error handling to show error if vans data is not shown.

---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+export default function Error() {
+
+    const error = useRouteError()
+
+    return(
+        <>
+        <h1>Error : {error.message}</h1>
+        <h3>{error.status} - {error.statusText}</h3>
+        </>
+    )
+}
