32d7e5b1f53313d58e5fd4a98d7ba8c62dcba9ae
Ayush1M <worklife2442@gmail.com>
2023-06-02 17:56:57 +0530

    Added login page and loader data

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import {createBrowserRouter, createRoutesFromElements,RouterProvider, Route} fro
 import Home from "./assets/Home"
 import About from "./assets/About"
 import Vans, {loader as vansLoader} from "./assets/Vans"
-import Vandetail from "./assets/Vandetail"
+import Vandetail, {loader as vanDetailLoader} from "./assets/Vandetail"
 import Layout from "./components/Layout"
 import Hostlayout from "./components/Hostlayout"
 import Dashboard from "./Host/Dashboard"
 import Income from "./Host/Income"
 import Reviews from "./Host/Reviews"
-import HostVans from "./Host/HostVans"
-import HostVanDetail from "./Host/HostVanDetail"
+import HostVans, {loader as HostVansLoader} from "./Host/HostVans"
+import HostVanDetail, {loader as HostVanDetailLoader} from "./Host/HostVanDetail"
 import HostVanInfo from "./Host/HostVanInfo"
 import HostVanPricing from "./Host/HostVanPricing"
 import HostVanPhotos from "./Host/HostVanPhotos"
 import NotFound from "./assets/NotFound"
 import Error from "./components/Error"
+import Login from "./assets/Login"
 
 import "./server"
 
@@ -26,14 +27,22 @@ function App() {
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />}/>
       <Route path="about" element={<About />}/>
-      <Route path="vans" element={<Vans />} errorElement={ <Error /> } loader={vansLoader}  />
-      <Route path="/vans/:id" element={<Vandetail />}/>
+      <Route path= "Login" element={<Login /> } />
+      <Route path="vans" 
+             element={<Vans />} 
+             errorElement={ <Error /> } 
+             loader={vansLoader}  />
+
+      <Route path="/vans/:id" 
+             element={<Vandetail />} 
+             errorElement={ <Error /> } 
+             loader={vanDetailLoader}/>
 
       <Route path="host" element={<Hostlayout />}>
         <Route index element={<Dashboard />} />
         <Route path="income" element={<Income />} />
-        <Route path="vans" element={<HostVans />} />
-        <Route path="vans/:id" element={<HostVanDetail />}>
+        <Route path="vans" element={<HostVans />}  loader={HostVansLoader}/>
+        <Route path="vans/:id" element={<HostVanDetail />} loader={HostVanDetailLoader} >
         <Route index element={<HostVanInfo />} />
         <Route path="pricing" element={<HostVanPricing />} />
         <Route path="photos" element={<HostVanPhotos />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import avatarImg from "../assets/images/avatar-icon.png"
 
 export default function Header(){
     return(
         <header>
           <Link className="site-logo" to="/">#vanlife</Link>
           <nav>
-            <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
-            <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
-            <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
+            <NavLink to="about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
+            <NavLink to="vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
+            <NavLink to="host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
+            <Link to="login" className="login-link"><img src={avatarImg} className="login-icon" /></Link>
           </nav>
        </header>
     )
