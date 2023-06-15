c24540e7e3b664deb5edf06472fc24cd733430d2
Ayush1M <worklife2442@gmail.com>
2023-06-15 23:28:54 +0530

    added local storage to login and fake log out.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ import { Link, NavLink } from "react-router-dom";
 import avatarImg from "../assets/images/avatar-icon.png"
 
 export default function Header(){
+
+  function fakeLogOut(){
+    localStorage.removeItem("loggedin")
+  }
+
     return(
         <header>
           <Link className="site-logo" to="/">#vanlife</Link>
@@ -11,6 +16,7 @@ export default function Header(){
             <NavLink to="vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
             <NavLink to="host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
             <Link to="login" className="login-link"><img src={avatarImg} className="login-icon" /></Link>
+            <button onClick={fakeLogOut}>Log Out</button>
           </nav>
        </header>
     )
