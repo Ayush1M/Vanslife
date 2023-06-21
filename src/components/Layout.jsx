a8df8a57c1bfeae8370d993afe5c5ede3c14c517
Ayush1M <worklife2442@gmail.com>
2023-06-22 00:00:04 +0530

    changes in classnames

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout(){
     return(
-        <>
+        <div className="wrapper">
         <Header />
-        <Outlet />
+        <main>
+            <Outlet />
+        </main>
         <Footer />
-        </>
+        </div>
     )
 }
